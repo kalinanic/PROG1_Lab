b8b25dbb3fe5452cadcbfab89c604855f2b80f29
kalinanic <37249521+kalinanic@users.noreply.github.com>
2018-03-25 08:37:17 +0200

    Add files via upload

---
+++ PROG1_Lab4.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, Perl, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    double temp;
+    printf("W celu sprawdzenia stanu skupienia wody wpisz dowolną temperaturę wyrażoną w stopniach Celsjusza:\n");
+    scanf("%lf", &temp);
+    if (temp >= 0 && temp < 100) {
+        printf("Woda w temperaturze %lf stopni Celsjusza jest w stanie ciekłym.\n", temp);
+    } if (temp >= 100) {
+        printf("Woda w temperaturze %lf stopni Celsjusza jest w stanie gazowym.\n", temp);
+    }
+    if (temp <= 0) { 
+        printf("Woda w temperaturze %lf stopni Celsjusza jest w stanie stałym.\n", temp);
+    }
+
+    return 0;
+}
