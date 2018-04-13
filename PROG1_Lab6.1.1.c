649ab190a668d6f4af126b23682b5e06d7c2d12f
kalinanic <37249521+kalinanic@users.noreply.github.com>
2018-04-13 11:58:13 +0200

    Update PROG1_Lab6.1.1.c

--- PROG1_Lab6.1.1.c
+++ PROG1_Lab6.1.1.c
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, Perl, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #define KELVIN 273
 
